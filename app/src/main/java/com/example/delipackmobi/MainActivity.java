ae4c057ccd04030fb8532cdd49a5e9783f48040d
Gh0stM0de <=jdotcole233@gmail.com>
2019-03-31 01:47:34 +0000

    first commit

---
+++ app/src/main/java/com/example/delipackmobi/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.delipackmobi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signin_activity);
+    }
+}
