de6dd88fdf658273b4a6fb239ce33286a5ef3084
Gh0stM0de <=jdotcole233@gmail.com>
2019-04-17 06:28:56 +0000

    login working

--- app/src/main/java/com/example/delipackmobi/SigninActivity.java
+++ app/src/main/java/com/example/delipackmobi/SigninActivity.java
@@ -9,6 +9,13 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 public class SigninActivity extends AppCompatActivity {
 
@@ -16,7 +23,7 @@ public class SigninActivity extends AppCompatActivity {
     private EditText customerPhoneNumber, customerPassword;
     private AsyncHttpClient asyncHttpClient;
     private DeliPackAlert deliPackAlert;
-    private static final String LOGIN_URL = "";
+    private static final String LOGIN_URL = "http://192.168.100.7:8000/customer_login";
 
 
 
@@ -28,6 +35,7 @@ public class SigninActivity extends AppCompatActivity {
         customerSignin = findViewById(R.id.customerSigninButton);
         customerPhoneNumber = findViewById(R.id.customerPhoneNumber);
         customerPassword = findViewById(R.id.customerPassword);
+        asyncHttpClient = new AsyncHttpClient();
 
 
 
@@ -45,6 +53,54 @@ public class SigninActivity extends AppCompatActivity {
                     String message = "Invalid phone number detected. Make sure your phone number is 10 digits";
                     new DeliPackAlert(SigninActivity.this, "Wrong phone number", message).showDeliPackAlert();
                     return;
+                } else {
+                    RequestParams requestParams = new RequestParams();
+                    requestParams.add("phone_number", customerphone);
+                    requestParams.add("password", customerpassword);
+                    System.out.println("Loading");
+
+                    asyncHttpClient.post(LOGIN_URL, requestParams, new JsonHttpResponseHandler(){
+                        @Override
+                        public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                            super.onSuccess(statusCode, headers, response);
+
+                            try {
+                                String response_validate = response.getString("success_cue");
+                                if (response_validate.contains("Success")){
+//                                    new DeliPackAlert(SigninActivity.this, "Success", "Successful").showDeliPackAlert();
+                                    startActivity(new Intent(SigninActivity.this, Homedashboard_user.class));
+                                    finish();
+
+                                } else if (response_validate.contains("Deactivated")){
+                                    String message = "Sorry!! This account has been deactivated. Contact us at 'https://delipack.com/contact' to resolve any issues";
+                                    new DeliPackAlert(SigninActivity.this, "Deactivated",  message).showDeliPackAlert();
+
+                                } else if (response_validate.contains("Failed")){
+                                    String message = "Attempt to login failed, check credentials and try again";
+                                    new DeliPackAlert(SigninActivity.this, "Login Failure", message).showDeliPackAlert();
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+
+                        @Override
+                        public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                            super.onFailure(statusCode, headers, throwable, errorResponse);
+                            System.out.println(errorResponse);
+                        }
+
+                        @Override
+                        public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                            super.onFailure(statusCode, headers, responseString, throwable);
+                            System.out.println(responseString);
+                        }
+
+                    });
+
+
+
                 }
             }
         });
@@ -59,7 +115,7 @@ public class SigninActivity extends AppCompatActivity {
         back_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent back_home_intent = new Intent(SigninActivity.this, Homedashboard_user.class);
+                Intent back_home_intent = new Intent(SigninActivity.this, MainActivity.class);
                 startActivity(back_home_intent);
                 finish();
             }
