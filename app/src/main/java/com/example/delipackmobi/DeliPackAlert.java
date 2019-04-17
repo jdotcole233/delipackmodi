94df3c47c22bc3da587b6047f93c2d9e26970694
Gh0stM0de <=jdotcole233@gmail.com>
2019-04-17 04:42:24 +0000

    alert and login

---
+++ app/src/main/java/com/example/delipackmobi/DeliPackAlert.java
@@ -0,0 +1,44 @@
+package com.example.delipackmobi;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+public class DeliPackAlert {
+    private Context context;
+    private String alertTitle;
+    private String message;
+    private LayoutInflater layoutInflater;
+
+    public DeliPackAlert(Context context, String title, String message){
+            this.context = context;
+            alertTitle = title;
+            this.message = message;
+            layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+
+    public void showDeliPackAlert(){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        View view = layoutInflater.inflate(R.layout.delipack_custom_alert, null);
+        TextView titletextView = view.findViewById(R.id.alertTitle);
+        TextView messagetextView = view.findViewById(R.id.alertMessage);
+        titletextView.setText(alertTitle);
+        messagetextView.setText(message);
+
+        builder.setView(view).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.create();
+        builder.show();
+    }
+
+}
--- app/src/main/java/com/example/delipackmobi/SigninActivity.java
+++ app/src/main/java/com/example/delipackmobi/SigninActivity.java
@@ -6,16 +6,55 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.loopj.android.http.AsyncHttpClient;
 
 public class SigninActivity extends AppCompatActivity {
 
-    private Button back_btn;
+    private Button back_btn, customerSignin;
+    private EditText customerPhoneNumber, customerPassword;
+    private AsyncHttpClient asyncHttpClient;
+    private DeliPackAlert deliPackAlert;
+    private static final String LOGIN_URL = "";
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signin_activity);
         back_btn = findViewById(R.id.backtohomebtn);
+        customerSignin = findViewById(R.id.customerSigninButton);
+        customerPhoneNumber = findViewById(R.id.customerPhoneNumber);
+        customerPassword = findViewById(R.id.customerPassword);
+
+
+
+        customerSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String customerphone = customerPhoneNumber.getText().toString();
+                String customerpassword = customerPassword.getText().toString();
+
+                if (customerphone.isEmpty() || customerpassword.isEmpty()){
+                    String message = "Ensure all fields are field before clicking the sign in button";
+                    new DeliPackAlert(SigninActivity.this, "Mandatory login fields", message).showDeliPackAlert();
+                    return;
+                } else if (customerphone.length() < 10){
+                    String message = "Invalid phone number detected. Make sure your phone number is 10 digits";
+                    new DeliPackAlert(SigninActivity.this, "Wrong phone number", message).showDeliPackAlert();
+                    return;
+                }
+            }
+        });
+
+
+
+
+
+
+
 
         back_btn.setOnClickListener(new View.OnClickListener() {
             @Override
