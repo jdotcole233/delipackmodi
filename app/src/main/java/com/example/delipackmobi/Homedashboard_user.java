9a1c0e010400608efd9b05f1d7105375d2ceb8e0
Gh0stM0de <=jdotcole233@gmail.com>
2019-04-24 12:07:04 +0000

    map work progress

--- app/src/main/java/com/example/delipackmobi/Homedashboard_user.java
+++ app/src/main/java/com/example/delipackmobi/Homedashboard_user.java
@@ -60,6 +60,8 @@ public class Homedashboard_user extends AppCompatActivity {
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        switchFragments(searchRiderFragment);
+
     }
 
 
--- app/src/main/java/com/example/delipackmobi/SearchRiderFragment.java
+++ app/src/main/java/com/example/delipackmobi/SearchRiderFragment.java
@@ -1,10 +1,13 @@
 package com.example.delipackmobi;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.CardView;
 import android.util.Log;
@@ -21,7 +24,19 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.delipackmobi.Model.PickUpDeliveryModel;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.api.net.PlacesClient;
@@ -38,8 +53,7 @@ import java.util.List;
 import static android.app.Activity.RESULT_OK;
 
 
-public class SearchRiderFragment extends Fragment {
-
+public class SearchRiderFragment extends Fragment implements OnMapReadyCallback {
 
 
     AutocompleteSupportFragment autocompleteFragment;
@@ -51,25 +65,25 @@ public class SearchRiderFragment extends Fragment {
     private TextView confirm_textview;
     private Spinner paymentSpinner;
     private String payment_selection;
-//    private Animation animationout;
+    //    private Animation animationout;
+    private MapView mapView;
+    private GoogleMap map;
 
     public SearchRiderFragment() {
         // Required empty public constructor
     }
 
 
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_search_rider, container, false);
-
-
 
 
     }
@@ -83,9 +97,38 @@ public class SearchRiderFragment extends Fragment {
         animationout = AnimationUtils.loadAnimation(getContext(), R.anim.slideout);
         animationmove = AnimationUtils.loadAnimation(getContext(), R.anim.move);
 
+
         rider_search_btn = getActivity().findViewById(R.id.rider_search);
         pickUpDeliveryModel = new PickUpDeliveryModel();
         resultcard = getActivity().findViewById(R.id.search_result_cardview);
+
+        MapsInitializer.initialize(getActivity());
+
+        switch (GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(getActivity())) {
+
+            case ConnectionResult.SUCCESS:
+                Log.i("delipack", "Success");
+                mapView = getActivity().findViewById(R.id.customermap);
+                mapView.onCreate(savedInstanceState);
+                if (mapView == null) {
+                    SupportMapFragment supportMapFragment = (SupportMapFragment) getActivity().getSupportFragmentManager().findFragmentById(R.id.customermap);
+                    supportMapFragment.getMapAsync(this);
+                }
+                break;
+            case ConnectionResult.SERVICE_MISSING:
+                Log.i("delipack", "Missing");
+                break;
+            case ConnectionResult.SERVICE_VERSION_UPDATE_REQUIRED:
+                Log.i("delipack", "Updated");
+                break;
+            default:
+                Log.i("delipack", "Default");
+
+
+        }
+
+
+//        mapView = getActivity().findViewById(R.id.customermap);
 //        confirm_button = getActivity().findViewById(R.id.confirm_btn);
 //        confirmcardview = getActivity().findViewById(R.id.confirm_cardview);
 //        confirm_payment = getActivity().findViewById(R.id.confirm_payment);
@@ -93,8 +136,8 @@ public class SearchRiderFragment extends Fragment {
 //        make_payment = getActivity().findViewById(R.id.make_payment);
 //        paymentSpinner = getActivity().findViewById(R.id.payment_choice);
 
-
-
+//        SupportMapFragment supportMapFragment = (SupportMapFragment) getActivity().getSupportFragmentManager().findFragmentById(R.id.customermap);
+//        supportMapFragment.getMapAsync(this);
 
 
         rider_search_btn.setOnClickListener(new View.OnClickListener() {
@@ -102,13 +145,15 @@ public class SearchRiderFragment extends Fragment {
             public void onClick(View v) {
 
 
-                if (pickUpDeliveryModel != null){
+                if (pickUpDeliveryModel != null) {
 
-                    if (pickUpDeliveryModel.getFromInformation() == null){
-                        Toast.makeText(getActivity(), "You need to fill a Pick up location", Toast.LENGTH_SHORT).show();
+                    if (pickUpDeliveryModel.getFromInformation() == null) {
+//                        Toast.makeText(getActivity(), "You need to fill a Pick up location", Toast.LENGTH_SHORT).show();
+                        new DeliPackAlert(getActivity(), "Location Fields", "Fill out pick up locations").showDeliPackAlert();
                         return;
-                    } else if (pickUpDeliveryModel.getDeliveryInformation() == null){
-                        Toast.makeText(getContext(), "Delivery information cannot be empty", Toast.LENGTH_SHORT).show();
+                    } else if (pickUpDeliveryModel.getDeliveryInformation() == null) {
+//                        Toast.makeText(getContext(), "Delivery information cannot be empty", Toast.LENGTH_SHORT).show();
+                        new DeliPackAlert(getActivity(), "Location Fields", "Delivery information cannot be empty").showDeliPackAlert();
                         return;
                     } else {
                         Toast.makeText(getActivity(), "Everything is good", Toast.LENGTH_SHORT).show();
@@ -118,15 +163,15 @@ public class SearchRiderFragment extends Fragment {
                     }
 
 
-                    for(String a: pickUpDeliveryModel.getDeliveryInformation().values())
-                        Log.i("delipack",  a.toString());
+                    for (String a : pickUpDeliveryModel.getDeliveryInformation().values())
+                        Log.i("delipack", a.toString());
 
-                    for(String a: pickUpDeliveryModel.getFromInformation().values())
-                        Log.i("delipack",  a.toString());
+                    for (String a : pickUpDeliveryModel.getFromInformation().values())
+                        Log.i("delipack", a.toString());
 
                 } else {
-                    Toast.makeText(getActivity(), "Fill out both forms to find rider", Toast.LENGTH_SHORT).show();
-
+//                    Toast.makeText(getActivity(), "Fill out both forms to find rider", Toast.LENGTH_SHORT).show();
+                    new DeliPackAlert(getActivity(), "Location Fields", "Fill out both forms to find rider").showDeliPackAlert();
                     return;
                 }
 
@@ -188,15 +233,6 @@ public class SearchRiderFragment extends Fragment {
 //        });
 
 
-
-
-
-
-
-
-
-
-
         // Initialize the AutocompleteSupportFragment.
         autocompleteFragment = (AutocompleteSupportFragment)
                 getChildFragmentManager().findFragmentById(R.id.autocomplete_fragment);
@@ -210,12 +246,12 @@ public class SearchRiderFragment extends Fragment {
             @Override
             public void onPlaceSelected(Place place) {
                 HashMap<String, String> place_from = new HashMap<>();
-                if (place != null){
-                    place_from.put(place.getId().toString(),place.getName().toString());
+                if (place != null) {
+                    place_from.put(place.getId().toString(), place.getName().toString());
                     pickUpDeliveryModel.setFromInformation(place_from);
                     Log.i("g", "Place: " + place.getName() + ", " + place.getId());
 
-                } else{
+                } else {
                     place_from = null;
                 }
                 // TODO: Get info about the selected place.
@@ -231,7 +267,7 @@ public class SearchRiderFragment extends Fragment {
 
 // Initialize the AutocompleteSupportFragment.
         autocompleteFragment1 = (AutocompleteSupportFragment)
-               getChildFragmentManager().findFragmentById(R.id.autocomplete_fragment1);
+                getChildFragmentManager().findFragmentById(R.id.autocomplete_fragment1);
 
 // Specify the types of place data to return.
         autocompleteFragment1.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
@@ -241,12 +277,12 @@ public class SearchRiderFragment extends Fragment {
             @Override
             public void onPlaceSelected(Place place) {
                 HashMap<String, String> place_delivery = new HashMap<>();
-                if (place != null){
-                    place_delivery.put(place.getId().toString(),place.getName().toString());
+                if (place != null) {
+                    place_delivery.put(place.getId().toString(), place.getName().toString());
                     pickUpDeliveryModel.setDeliveryInformation(place_delivery);
                     Log.i("g", "Place: " + place.getName() + ", " + place.getId());
 
-                } else{
+                } else {
                     place_delivery = null;
                 }
             }
@@ -259,8 +295,34 @@ public class SearchRiderFragment extends Fragment {
         });
 
 
-
     }
 
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        map = googleMap;
+
+        LatLng sydney = new LatLng(5.6037, 0.1870);
+        map.addMarker(new MarkerOptions().position(sydney)
+                .title("Marker in Sydney"));
+//        googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+//        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return;
+//        }
+//        googleMap.setMyLocationEnabled(true);
+//        googleMap.setTrafficEnabled(true);
+//        googleMap.setIndoorEnabled(true);
+//        googleMap.setBuildingsEnabled(true);
+//        googleMap.getUiSettings().setZoomControlsEnabled(true);
+
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
 }
