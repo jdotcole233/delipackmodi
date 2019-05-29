d98459864f56c76f83e1cf0aa7d530446fd0d5e7
Gh0stM0de <=jdotcole233@gmail.com>
2019-05-29 09:39:02 +0000

    contact page

---
+++ app/src/main/java/com/example/delipackmobi/RiderPickedUpInformation.java
@@ -0,0 +1,13 @@
+package com.example.delipackmobi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RiderPickedUpInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rider_picked_up_information);
+    }
+}
