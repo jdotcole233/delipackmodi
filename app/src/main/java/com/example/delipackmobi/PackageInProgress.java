231bdeb1850bdc28a7bcf00899cd4dde5be938d9
Gh0stM0de <=jdotcole233@gmail.com>
2019-05-29 16:47:22 +0000

    done with UI for transport

---
+++ app/src/main/java/com/example/delipackmobi/PackageInProgress.java
@@ -0,0 +1,13 @@
+package com.example.delipackmobi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PackageInProgress extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_package_in_progress);
+    }
+}
---
+++ app/src/main/java/com/example/delipackmobi/RateCompanyRider.java
@@ -0,0 +1,13 @@
+package com.example.delipackmobi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RateCompanyRider extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rate_company_rider);
+    }
+}
