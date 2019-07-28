fc2597b1dcb42dd573e247e1a294574e3c01d01e
Gh0stM0de <=jdotcole233@gmail.com>
2019-07-28 20:18:23 +0000

    promotion validation

--- app/src/main/java/com/delipackport/delipackmobi/PromotionsView.java
+++ app/src/main/java/com/delipackport/delipackmobi/PromotionsView.java
@@ -4,12 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 public class PromotionsView extends AppCompatActivity {
 
     private ImageView close_promoview;
     private Button promotionButton;
+    private EditText promotTextEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +20,20 @@ public class PromotionsView extends AppCompatActivity {
 
         close_promoview = findViewById(R.id.editpromo_close);
         promotionButton = findViewById(R.id.promotionButton);
+        promotTextEdit = findViewById(R.id.promotTextEdit);
 
         promotionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String message = "Sorry, the promotion code entered is invalid at this time, check the code and try again";
+                String promotionText = promotTextEdit.getText().toString();
+                String message;
+
+                if (promotionText.isEmpty()){
+                    message = "Please enter a valid promotion code";
+                } else {
+                    message = "Sorry, the promotion code entered is invalid at this time, check the code and try again";
+                }
+
                 new DeliPackAlert(PromotionsView.this, "Promotion!!", message).showDeliPackAlert();
                 return;
             }
